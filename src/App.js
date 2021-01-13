74893f3c7488d576066d7701646220c10707db24
chimdi <nzechimdi@yahoo.com>
2021-01-13 23:11:03 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ export default class App extends React.Component{
        <div className='user'>
           <img src={this.state.user.picture.large} alt='user--img' className='user--img' />
           <div className='user--info'>
-            <h3 className='user--name'> 
-              {`${this.state.user.name.first}  ${this.state.user.name.last}`} 
-            </h3>
+            <h3 className='user--name'> {`${this.state.user.name.first}  ${this.state.user.name.last}`} </h3>
             <hr></hr>
-            <p className='user--email'>EMAIL: {this.state.user.email} </p>
+            <p className='user--email'> <span>EMAIL:</span> {this.state.user.email} </p>
             <hr></hr>
-            <p className='user--location'>RESIDENCE: {`${this.state.user.location.city}, ${this.state.user.location.country}`} </p>
+            <p className='user--age'><span>AGE:</span> {this.state.user.dob.age} </p>
+            <hr></hr>
+            <p className='user--location'><span>RESIDENCE:</span> {`${this.state.user.location.city}, ${this.state.user.location.country}`} </p>
           </div>
         </div>
        }
